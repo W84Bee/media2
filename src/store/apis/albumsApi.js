892fdcad9a3f74c6c3b68131754e7a1508059486
Brandon Alexander <bjvonalexander@gmail.com>
2023-06-15 13:56:02 -0500

    Modern Async with Redux Toolkit
    - Creating an Endpoint to fetch Albums for a user.
    - Using the Generated Hook

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -5,5 +5,22 @@ const albumsApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3005'
     }),
-    
+    endpoints(build) {
+        return {
+            fetchAlbums: build.query({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id
+                        },
+                        method: 'GET'
+                    };
+                }
+            })
+        };
+    }
 });
+
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
